c5a594a96efa1d913b53e92001951006ebc685d7
dunggamja <dunggamja@gmail.com>
2025-10-06 01:16:59 +0900

    타일때문에 계속 삽질하고 지우고

--- fe_client/Assets/Script/Battle/Terrain/Tile/TerrainTileOverlay.cs
+++ fe_client/Assets/Script/Battle/Terrain/Tile/TerrainTileOverlay.cs
@@ -16,6 +16,10 @@ public class TerrainTileOverlay : MonoBehaviour
     private MeshFilter m_mesh_filter = null;
 
 
+    [SerializeField]
+    private MeshRenderer m_mesh_renderer = null;
+
+
     // [SerializeField]
     // private MeshRenderer m_mesh_renderer = null;
     // [SerializeField]
@@ -70,57 +74,31 @@ public class TerrainTileOverlay : MonoBehaviour
         Debug.Log($"heightMap: {heightMap.GetLength(0)}, {heightMap.GetLength(1)}");
 
         // 메시 생성
-        var mesh = CreateMeshFromHeightMap(heightMap, terrainSize, terrainPosition);
+        CreateMeshFromHeightMap(heightMap, terrainSize, terrainPosition);
 
-        if (mesh != null && m_mesh_filter != null)
-        {
-            m_mesh_filter.mesh = mesh; 
-        }
 
-        // // 메시 필터에 할당
-        // if (m_mesh_filter != null)
-        // {
-        //     m_mesh_filter.mesh = m_generated_mesh;
-        // }
+        // 텍스쳐 생성
+        CreateTextureFromTilemap();
+        
 
-        // // 머티리얼 할당
-        // if (m_mesh_renderer != null && m_terrain_material != null)
-        // {
-        //     m_mesh_renderer.material = m_terrain_material;
-        // }
-
-        // Debug.Log($"Terrain mesh generated with {m_vertices.Length} vertices and {m_triangles.Length / 3} triangles");
+       
     }
 
      /// <summary>
     /// 높이맵으로부터 메시 생성
     /// </summary>
-    private Mesh CreateMeshFromHeightMap(float[,] heightMap, Vector3 terrainSize, Vector3 terrainPosition)
+    private void CreateMeshFromHeightMap(float[,] heightMap, Vector3 terrainSize, Vector3 terrainPosition)
     {
         int width  = heightMap.GetLength(1);
         int height = heightMap.GetLength(0);
 
-        Debug.Log($"heightmap width: {width}, height: {height}");
+        // Debug.Log($"heightmap width: {width}, height: {height}");
 
         // 버텍스 배열 초기화
         var vertices  = new List<Vector3>(width * height);
         var uvs       = new List<Vector2>(width * height);
         var normals   = new List<Vector3>(width * height);
         var triangles = new List<int>(width * height * 2 * 3);
-
-
-        // var heightmap_multiplier = 1f;
-
-        // var max_zpos = 0f;
-        // var max_xpos = 0f;
-
-        // width = 4;
-        // height = 4;
-
-
-        var x_step = terrainSize.x / (width - 1);
-        var z_step = terrainSize.z / (height - 1);
-
 
         // 버텍스 생성
         for (int y = 0; y < height; y++)
@@ -131,13 +109,9 @@ public class TerrainTileOverlay : MonoBehaviour
                 var pos_z = (float)y / (height - 1) * terrainSize.z;
                 var pos_y = heightMap[y, x] * terrainSize.y;
 
-                vertices.Add(new Vector3(pos_x, pos_y, pos_z) + terrainPosition);
-                
+                vertices.Add(new Vector3(pos_x, pos_y, pos_z) + terrainPosition);                
                 normals.Add(Vector3.up);
-
-
-                uvs.Add(new Vector2((float)x / (width - 1), (float)y / (height - 1)));
-               
+                uvs.Add(new Vector2((float)x / (width - 1), (float)y / (height - 1)));               
             }
         }
 
@@ -166,81 +140,125 @@ public class TerrainTileOverlay : MonoBehaviour
         generated_mesh.normals     = normals.ToArray();
         generated_mesh.RecalculateBounds();
 
+        if (generated_mesh != null && m_mesh_filter != null)
+        {
+            m_mesh_filter.mesh = generated_mesh; 
+        }
+    }
+
+    void CreateTextureFromTilemap()
+    {
+        if (m_tilemap == null)
+            return;
+
+        const int TILE_SIZE = 16;
+        
+        var texture        = new Texture2D(256, 256, TextureFormat.RGBA32, false);
+        texture.filterMode = FilterMode.Point;
+        texture.wrapMode   = TextureWrapMode.Clamp;
+
+
+
+
+
+        var tiles = new HashSet<TileBase>();
+        var tilemap_bounds = m_tilemap.cellBounds;
+        for (int z = tilemap_bounds.zMin; z < tilemap_bounds.zMax; z++)
+        {
+            for (int x = tilemap_bounds.xMin; x < tilemap_bounds.xMax; x++)
+            {
+                var tile = m_tilemap.GetTile(new Vector3Int(x, 0, z));
+                if (tile != null && !tiles.Contains(tile))
+                    tiles.Add(tile);
+            }
+        }
+
+            // 모든 타일을 순회하며 특정 타입만 삭제
+        for (int y = tilemap_bounds.yMin; y < tilemap_bounds.yMax; y++)
+        {
+            for (int x = tilemap_bounds.xMin; x < tilemap_bounds.xMax; x++)
+            {
+                for (int z = tilemap_bounds.zMin; z < tilemap_bounds.zMax; z++)
+                {
+                    Vector3Int cellPos = new Vector3Int(x, y, z);
+                    TileBase tile = m_tilemap.GetTile(cellPos);
+
+                    Debug.Log($"tile: {tile}, cellPos: {cellPos}");    
+                }
+            }
+        }
+        
+
+
+
+
+        Debug.Log($"tiles: {tiles.Count}");
+        Debug.Log($"tilemap_bounds: {tilemap_bounds}");
+
+
+        var tile_per_row = texture.width / TILE_SIZE;
+        var tile_index   = 0;
+        
+
+        foreach (var tile in tiles)
+        {
+            var rule_tile = tile as RuleTile;
+            if (rule_tile == null || rule_tile.m_DefaultSprite == null)
+                continue;
+
+            var tile_sprite = rule_tile.m_DefaultSprite;
+            if (tile_sprite == null || tile_sprite.texture == null)
+                continue;
+
+            var tile_texture = tile_sprite.texture;
+            var tile_rect    = tile_sprite.rect;
+
+            var texture_x = (tile_index % tile_per_row) * TILE_SIZE;
+            var texture_y = (tile_index / tile_per_row) * TILE_SIZE;
+
+            for (int y = 0; y < tile_rect.height; y++)
+            {
+                for (int x = 0; x < tile_rect.width; x++)
+                {
+                    var u = tile_rect.x + x / (float)TILE_SIZE * tile_rect.width;
+                    var v = tile_rect.y + y / (float)TILE_SIZE * tile_rect.height;
+
+                    texture.SetPixel(texture_x + x, texture_y + y, tile_texture.GetPixel(x, y));
+                }
+            }
+
+            tile_index++;            
+        }
+
+
+
 
         
 
-        return generated_mesh;
-    }
 
-
-        /// <summary>
-    /// 노멀 벡터 계산
-    /// </summary>
-    private void CalculateNormals(int width, int height, Vector3[] vertices, Vector3[] normals)
-    {
-        for (int y = 0; y < height; y++)
+        if(m_mesh_renderer != null)
         {
-            for (int x = 0; x < width; x++)
+            if (Application.isPlaying)
             {
-                int index = y * width + x;
-                normals[index] = Vector3.up;
-                // Vector3 normal = Vector3.zero;
-
-                // // 주변 버텍스들과의 차이를 이용해 노멀 계산
-                // if (x > 0 && x < width - 1 && y > 0 && y < height - 1)
-                // {
-                //     Vector3 left = vertices[index - 1];
-                //     Vector3 right = vertices[index + 1];
-                //     Vector3 up = vertices[index - width];
-                //     Vector3 down = vertices[index + width];
-
-                //     Vector3 horizontal = right - left;
-                //     Vector3 vertical = down - up;
-
-                //     normal = Vector3.Cross(horizontal, vertical).normalized;
-                // }
-                // else
-                // {
-                //     normal = Vector3.up; // 경계에서는 위쪽 노멀 사용
-                // }
-
-                // normals[index] = normal;
+                if (m_mesh_renderer.material != null)
+                {
+                    m_mesh_renderer.material.mainTexture = texture;
+                }
             }
-        }
-    }
-
-    /// <summary>
-    /// 트라이앵글 인덱스 생성
-    /// </summary>
-    private List<int> GenerateTriangles(int width, int height)
-    {
-        List<int> triangles = new List<int>();
-
-        for (int y = 0; y < height - 1; y++)
-        {
-            for (int x = 0; x < width - 1; x++)
+            else
             {
-                int index_bl = y * width + x;
-                int index_br = index_bl  + 1;
-
-                int index_tl = index_bl  + width;
-                int index_tr = index_tl  + 1;
-
-
-                // 첫 번째 트라이앵글 (시계방향)
-                triangles.Add(index_bl);
-                triangles.Add(index_tl);
-                triangles.Add(index_br);
-
-                // 두 번째 트라이앵글 (시계방향)
-                triangles.Add(index_br);
-                triangles.Add(index_bl);
-                triangles.Add(index_tr);
+                if (m_mesh_renderer.sharedMaterial != null)
+                {
+                    m_mesh_renderer.sharedMaterial.mainTexture = texture;
+                }
             }
         }
 
-        return triangles;
+
+        texture.Apply();
     }
+
+
 
 
 }
