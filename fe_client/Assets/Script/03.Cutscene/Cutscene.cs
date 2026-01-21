fd195cb3f3ae161e5864af0f484cfc1b449df7a4
dunggamja <dunggamja@gmail.com>
2026-01-22 00:45:29 +0900

    컷씬 매니저 작업 진행중

--- fe_client/Assets/Script/03.Cutscene/Cutscene.cs
+++ fe_client/Assets/Script/03.Cutscene/Cutscene.cs
@@ -5,26 +5,62 @@ using UnityEngine;
 using Lua;
 using Battle;
 using Cysharp.Threading.Tasks;
+using System.Threading;
 
 
-public enum EnumCutsceneAction
+public enum EnumCutsceneType
 {
     None      = 0,
 
     Wait      = 1,   // 대기
 
-    Dialogue  = 10,   // 대화 연출
+    Dialogue  = 10,  // 대화 연출
     Unit_Move = 20,
 }
 
-public abstract class Cutscene_Action
+public abstract class Cutscene
 {
-   public abstract EnumCutsceneAction Action { get; }
-   public abstract UniTask Execute();
+   public abstract EnumCutsceneType Type { get; }
+   public async UniTask Execute(CancellationToken _skip_token)
+   {
+      try
+      {
+         OnEnter();
+
+         if (_skip_token.IsCancellationRequested)
+            return;            
+
+         await OnUpdate(_skip_token);
+      }
+      catch (OperationCanceledException)
+      {
+         // 취소 처리 발생하여도 계속 진행하기 위함.
+      }
+      finally
+      {
+         OnExit();
+      }
+   }
+
+   protected abstract void    OnEnter();
+   protected abstract UniTask OnUpdate(CancellationToken _skip_token);
+   protected abstract void    OnExit();
 }
 
 
-public class Cutscene
+public class CutsceneTrack
 {
-   // List<Cutscene_Action> Actions
+   public string Name { get; private set; } = string.Empty;
+
 }
+
+public class CutsceneSequence
+{
+
+}
+
+
+// public class Cutscene
+// {
+//    // List<Cutscene_Action> Actions
+// }
---
+++ fe_client/Assets/Script/03.Cutscene/CutsceneTrack.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Lua;
+using Battle;
+using Cysharp.Threading.Tasks;
