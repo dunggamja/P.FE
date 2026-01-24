87038452138d067b2dc3ab71c17aef9398b1e789
dunggamja <dunggamja@gmail.com>
2026-01-24 13:12:52 +0900

    에셋들 폴더 정리.

---
+++ fe_client/Assets/ResourcesPrivate/03.terrain/demo/script/02.intro.lua
@@ -0,0 +1,9 @@
+﻿-- LOAD_AND_RUN이 필요할경우 아래 변수를 설정해야 한다.
+SCRIPT_MODULE      = "demo_script_02"
+SCRIPT_MODULE_FUNC = "Run"
+
+demo_script_02 = {}
+
+-- lua의 주석
+function demo_script_02.Run()    
+end
